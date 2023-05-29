c4e6647439354c2c1a956e5ca89ba3339a95cdfa
gich2009 <waneneamos@gmail.com>
2023-05-29 13:00:10 +0300

    Completed assignment

--- lib/Article.rb
+++ lib/Article.rb
@@ -7,11 +7,21 @@ class Article
   
   @@all = []
 
+
+  #The initializer also has a try-catch/ begin-raise-rescue block to handle errors when the class is initialized with unexpected data types.
   def initialize(author:, magazine:, title:)
-    @author = author 
-    @magazine = magazine
-    @title = title
-    @@all << self
+     begin
+      raise ArgumentError, "Name of #{self.class.name} must be a string" unless title.is_a?(String)
+      raise ArgumentError, "Magazine of #{self.class.name} must be a Magazine instance" unless magazine.is_a?(Magazine)
+      raise ArgumentError, "Author of #{self.class.name} must be an Author instance" unless author.is_a?(Author)
+      @author = author 
+      @magazine = magazine
+      @title = title
+      @@all << self
+    rescue ArgumentError => e
+      puts "Error: #{e.message}"
+    end
+    
   end
   
   def self.all
